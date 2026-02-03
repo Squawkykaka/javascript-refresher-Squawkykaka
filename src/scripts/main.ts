83dde5bb3c37e18abc7ef46f4127d8131df4e544
Squawkykaka <squawkykaka@gmail.com>
2026-02-03 22:32:56 +1300

    actual work done, extension time!

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { doc, getFirestore, setDoc } from "firebase/firestore";
+import { EmailAuthProvider, getAuth, linkWithCredential, onAuthStateChanged, signInAnonymously, signInWithEmailAndPassword } from "firebase/auth";
+import { addDoc, collection, doc, getFirestore, limit, onSnapshot, orderBy, query, serverTimestamp, setDoc, Timestamp } from "firebase/firestore";
 
 const firebaseConfig = {
 
@@ -17,34 +18,130 @@ const firebaseConfig = {
 
 };
 
+let message = document.getElementById("welcomeMessage")!;
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
+signInAnonymously(auth).catch(error => {
+    message.innerHTML = error
+})
 
-
-
-
-let message = document.getElementById("welcomeMessage")!;
-message.innerHTML = "Javascript Loaded";
-
-console.log("Test");
-
-document.getElementById("headingButton")!.addEventListener("click", event => {
-    message.innerHTML = "You pressed the button!";
-});
+let userName = "Anonymous";
 
 // changing header with a form
-let form: HTMLFormElement = document.getElementById("headingForm")!;
-form.addEventListener("submit", event => {
+let headerForm: HTMLFormElement = document.getElementById("headingForm")!;
+headerForm.addEventListener("submit", event => {
     event.preventDefault();
-    const data = new FormData(form);
+    const data = new FormData(headerForm);
     const newName = data.get("newHeadingName");
-
-    message.innerHTML = newName;
     updateMessage(newName);
 });
 
+/// function to update the header with a new name
 async function updateMessage(text: string) {
-    await setDoc(doc(db, "main", "note"), {
-        content: text
-    });
+    // this function is always called in a context where we know it is valid
+    let user = auth.currentUser!;
+    await addDoc(collection(db, "header"), {
+        title: text,
+        createdAt: serverTimestamp(),
+        userRef: doc(db, "users", user.uid),
+        userName: user.email
+    })
+
 }
+
+// when a change to the database is detected, pulls it and applies it to the header and update list.
+const unsubscribe = onSnapshot(query(collection(db, "header"), orderBy("createdAt", "desc"), limit(5)), (snapshot) => {
+    if (!snapshot.empty) {
+        // apply to header
+        const doc = snapshot.docs[0];
+        const data = doc.data();
+
+        let new_title = data.title;
+        message.innerHTML = new_title;
+
+        console.debug("New Title: ", new_title);
+
+        // apply to list
+        for (let i = 0; i < snapshot.docs.length; i++) {
+            const element = snapshot.docs[i];
+            const data = element.data();
+
+            let htmlElement = document.querySelector(`#changeList > #item${i + 1}`)!;
+            let createdDate: Timestamp = data.createdAt;
+            let date = createdDate.toDate();
+            let formattedDate = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+
+            let userName = "";
+            if (data.userName) { userName = data.userName } else { userName = "Anonymous" };
+            htmlElement.innerHTML = `
+            <p>${data.title}</p>
+            <p>${formattedDate}</p>
+            <p>${userName}</p>`;
+        }
+    }
+})
+
+// Signup
+let signupForm: HTMLFormElement = document.getElementById("signupForm")!;
+signupForm.addEventListener("submit", event => {
+    event.preventDefault();
+    const data = new FormData(signupForm);
+    const email = data.get("email");
+    const password = data.get("password");
+
+    const credential = EmailAuthProvider.credential(email, password);
+    linkWithCredential(auth.currentUser!, credential)
+        .then((usercred) => {
+            const user = usercred.user;
+            document.querySelector("#signupForm > p")!.innerHTML = "Success";
+
+            setDoc(doc(db, "users", user.uid), {
+                displayName: user.displayName,
+                email: user.email,
+            })
+        }).catch((error) => {
+            console.error("Error upgrading anonymous account", error);
+            document.querySelector("#signupForm > p")!.innerHTML = error;
+        });
+});
+
+// Login
+let loginForm: HTMLFormElement = document.getElementById("loginForm")!;
+loginForm.addEventListener("submit", event => {
+    event.preventDefault();
+    const data = new FormData(signupForm);
+    const email = data.get("email");
+    const password = data.get("password");
+
+    signInWithEmailAndPassword(auth, email, password).then(() => {
+        document.querySelector("#loginForm > p")!.innerHTML = "Success";
+
+    }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        console.log(errorCode, ": ", errorMessage);
+        document.querySelector("#loginForm > p")!.innerHTML = errorMessage;
+    });;
+});
+
+// This enables the heading update button, and sets the userName variable for easy access
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const uid = user.uid;
+
+        if (user.email) { userName = user.email };
+        document.getElementById("userName")!.innerHTML = userName;
+
+        console.log("Enabling form");
+        document.querySelector("#headingButton")!.removeAttribute("disabled");
+        // ...
+    } else {
+        // User is signed out
+        // ...
+    }
+});
